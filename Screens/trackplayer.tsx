78ba1fa3bc56dcb5ac41c3123bb09881d95b3805
Saboten758 <debjitdaw03.com>
2023-08-18 00:01:54 +0530

    fixed

--- Screens/trackplayer.tsx
+++ Screens/trackplayer.tsx
@@ -75,52 +75,35 @@ import TrackPlayer, {
         url: "http://radio.plaza.one/mp3",
         artist:"Nightwave Plaza: A tranquil oasis of lo-fi hip hop and chill beats.",
         title:'Night Wave Plaza Radio',
-        duration:0
+        duration:0,
+        isLiveStream:true,
       },
       {
         id:'2',
         url: "https://jenny.torontocast.com:2000/stream/J1GOLD",
         artist:"Japan's greatest hits of the 60s, 70s and 80s.",
         title:'J1 Gold Radio',
-        duration:0
+        duration:0,
+        isLiveStream:true,
       },
       {
         id:'3',
         url: "https://jenny.torontocast.com:2000/stream/J1XTRA",
         artist:"Playing the hits from the Heisei era (1989~2019)",
         title:'J1 Xtra Radio',
-        duration:0
+        duration:0,
+        isLiveStream:true,
       },
       {
         id:'4',
         url: "https://jenny.torontocast.com:2000/stream/J1HITS",
         artist:"Japan's Hottest Hits. Playing today's hits heard on Japanese FM radio.",
         title:'J1 Hits Radio',
-        duration:0
+        duration:0,
+        isLiveStream:true,
       },
   
-      //extras
-        // {
-        //   id: '1',
-        //   url: require('./assets/fluidity-100-ig-edit-4558.mp3'),
-        //   title: 'Fluidity',
-        //   artist: 'tobylane',
-        //   duration: 60,
-        // },
-        // {
-        //   id: '2',
-        //   url: require('./assets/penguinmusic-modern-chillout-future-calm-12641.mp3'),
-        //   title: 'Modern Chillout',
-        //   artist: 'penguinmusic',
-        //   duration: 66,
-        // },
-        // {
-        //   id: '3',
-        //   url: require('./assets/powerful-beat-121791.mp3'),
-        //   title: 'Powerful Beat',
-        //   artist: 'penguinmusic',
-        //   duration: 73,
-        // }
+      
       ]);
       await TrackPlayer.setRepeatMode(RepeatMode.Queue);
     
