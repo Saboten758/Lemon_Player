190f6640e5fc4b64b39c6ce3b4273b9e7e9a8b90
Saboten758 <debjitdaw03.com>
2023-09-18 01:48:12 +0530

    more_feeds

--- Screens/Explore.tsx
+++ Screens/Explore.tsx
@@ -23,7 +23,11 @@ const Explore=()=>{
     require('../assets/night.jpg'),require('../assets/mid.jpg')]
     const [x,setx]=useState("")
     const [head,setHead]=useState("")
-    const feeds=['https://www.animenewsnetwork.com/all/rss.xml?ann-edition=us','https://90sanime.com/feed/','https://prod-qt-images.s3.amazonaws.com/production/filmcompanion/feed.xml','https://lwlies.com/feed/','https://kotaku.com/rss']
+    const feeds=['https://www.animenewsnetwork.com/all/rss.xml?ann-edition=us','https://90sanime.com/feed/',
+    'https://prod-qt-images.s3.amazonaws.com/production/filmcompanion/feed.xml','https://lwlies.com/feed/',
+    'https://kotaku.com/rss','https://blog.google/technology/ai/rss/','https://towardsdatascience.com/feed',
+    'https://www.ign.com/rss/articles/feed?tags=games','https://hecooks.co/feed/','https://minimalistbaker.com/feed/',
+    'https://www.pcgamer.com/rss/']
    
    const a=()=>{
     fetch(feeds[curr]).then((response) => response.text())
@@ -50,12 +54,19 @@ const Explore=()=>{
 );
 
   const data = [
+   
     { label: 'Anime News Network', value: '0',data:'All anime& manga news' },
+    { label: 'Google AI Blog', value: '5',data:"The latest news on Google Research in Artificial Intelligence, machine learning, updates and more!" },
     { label: 'Kotaku', value: '4',data:'Kotaku is the definitive digital hub for video game news, reviews, cheats, design, and entertainment' },
     { label: '90s anime.com', value: '1',data:'90s anime blog feed' },
     { label: 'Film Companinion', value: '2',data:'Indian Films' },
     { label: 'Little White Lies', value: '3',data:"The world's most beautiful film magazine" },
     
+    { label: 'TDS', value: '6',data:"Sharing concepts, ideas and codes on data science." },
+    { label: 'IGN', value: '7',data:"IGN.com is your premier online destination for gaming, movies, comics and everything you're into" },
+    { label: 'He Cooks', value: '8',data:"Authentic and approachable dishes, cocktails, and more for anyone with a kitchen, a stomach, and a heart." },
+    { label: 'Minimalist Baker', value: '9',data:"Sharing simple, delicious plant-based and mostly gluten-free recipes" },
+    { label: 'PC Gamer', value: '10',data:"Sharing simple, delicious plant-based and mostly gluten-free recipes" }
   ];
   const [value, setValue] = useState(null);
     var key=0
@@ -83,8 +94,7 @@ const Explore=()=>{
     const hed=()=>(<View style={{backgroundColor:'#3D3C49',padding:10}}><Text style={styles.title}>{head}</Text></View>)
     const sep=()=>(<View style={{borderWidth:1,borderColor:'black'}}/>)
     
-   
-    
+
     return(
         <>
         <View style={styles.head}>
@@ -133,11 +143,16 @@ const Explore=()=>{
                 <Dropdown
         style={styles.dropdown}
         placeholderStyle={styles.txt3}
+        itemContainerStyle={{borderRadius:10}}
         selectedTextStyle={styles.txt3}
         inputSearchStyle={styles.txt4}
-        itemTextStyle={styles.txt3}
+        itemTextStyle={styles.txt66}
+        dropdownPosition={'bottom'}
+        containerStyle={{backgroundColor:'#3D3C49',borderRadius:10,elevation:5}}
         data={data}
         search
+        iconStyle={{borderColor:'black'}}
+        activeColor={'black'}
         maxHeight={300}
         labelField="label"
         valueField="value"
@@ -230,10 +245,15 @@ const styles=StyleSheet.create({
         fontFamily:'Caveat-VariableFont_wght',
         fontSize:25
     },
+    txt66:{
+      color:'white',
+      fontFamily:'Caveat-VariableFont_wght',
+      fontSize:25
+  },
     txt4:{
-        color:'black',
+        borderRadius:10,
         fontFamily:'Caveat-VariableFont_wght',
-        fontSize:20
+        fontSize:18
     },
     item: {
         padding: 10,
@@ -250,7 +270,7 @@ const styles=StyleSheet.create({
         margin: 16,
         height: 30,
         width:200,
-        
+      
         borderBottomColor: 'gray',
         borderBottomWidth: 0.5,
       },
